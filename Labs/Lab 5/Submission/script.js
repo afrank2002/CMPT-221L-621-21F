75dc2079219ce04e1984f5ba764c90c95f6093db
Austin Frank <austinfrank00@gmail.com>
2021-10-06 19:00:57 -0400

    lab 5

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("description").innerHTML = "Austin Frank!";
+document.getElementById("bossy").innerHTML = "Here are some things that would be nice if you did :)";
+document.getElementById("1").style.color = "blue";
+document.getElementById("2").style.backgroundColor = "#FFFDD0";
+document.body.style.backgroundColor = "grey";
+document.getElementById("4").style.textTransform = "UPPERCASE";
+
+  
